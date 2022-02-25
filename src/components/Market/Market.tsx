9ee03e62c14902c99ce12466d8340102c05bef33
udomsak <specialclub13@gmail.com>
2022-02-25 18:32:30 +0700

    add market

--- src/components/Market/Market.tsx
+++ src/components/Market/Market.tsx
@@ -1,47 +1,67 @@
 import React from 'react'
+import { MdLocationOn } from 'react-icons/md'
+import { FaGavel } from 'react-icons/fa'
 import '../Market/Market.scss'
 
 export default function Market() {
   return (
     <div id='market'>
       <div id="menu">
+        <div id="menuButton">
         <div className='for-buy'>For Buy</div>
         <div className='for-rent'>For Rent</div>
+        </div>
       </div>
-      <div id="marketText">NFTs Lands</div>
-      <div id="cardsBox">
-        <div id="cards">
-          <div className="image">
-            <img src="" alt="" />
-          </div>
-          <div className="land">
-
-          </div>
+      <div className='show-land'>
+        <div className='topic-div'>
+          <p className='topic-text'>NFTs Lands</p>
         </div>
-        <div id="cards">
-          <div className="image">
-            <img src="" alt="" />
+        <div className='auction-land-container'>
+          <div className='land-card'>
+            <div className='land-image-div'>
+              <img className='land-image' src="/land.png" alt="" />
+            </div>
+            <div className='land-detail'>
+              <div className='name-location'>
+                <div className='land-name'>
+                  <p className='land-name-text'>LAND (99, 199)</p>
+                </div>
+                <div className='location-div'>
+                  <MdLocationOn className='location-icon' />
+                  <p className='location'>X: 99, Y: 199</p>
+                </div>
+                <div className='wallet-div'>
+                  <p className='owner-wallet'>0xcc896c2cdd10abaea84da606344x3455u8gh366989836778256dgh33</p>
+                </div>
+              </div>
+              <div className="button">
+                <div className='button-buy'>Buy 0.5 eth</div>
+              </div>
+            </div>
           </div>
-          <div className="land">
-            
-          </div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
+          <div className='land-card'></div>
         </div>
-        <div id="cards">
-          <div className="image">
-            <img src="" alt="" />
-          </div>
-          <div className="land">
-            
-          </div>
-        </div>
-        <div id="cards">
-          <div className="image">
-            <img src="" alt="" />
-          </div>
-          <div className="land">
-            
-          </div>
-        </div>
+      </div>
+      <div className='pagination-container'>
+        <div className='pagination'></div>
       </div>
     </div>
   )
