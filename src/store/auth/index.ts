288a100b54768f969dbb0deecef5b78b94987622
Matupoom Chaiwan <matupoom001@gmail.com>
2022-02-26 17:21:57 +0700

    add data to cookie and localstorage when login logout

--- src/store/auth/index.ts
+++ src/store/auth/index.ts
@@ -1,27 +1,64 @@
-import { action, observable } from 'mobx'
+import { action, makeAutoObservable, observable } from 'mobx'
+import AccountModel from '../../models/auth/AccountModel'
+import { ethers } from 'ethers'
+import Cookies from 'js-cookie'
+import { createContext } from 'react'
 class AuthStore {
-  // @observable
-  // public googleUser: GoogleUserModel = new GoogleUserModel()
 
-  // @action
-  // public logout(): void {
-  //   if (Cookies.get('google_oauth2')) Cookies.remove('google_oauth2')
-  //   const timer: number = +setTimeout(() => 0)
-  //   for (let i = timer; i > 0; i--) {
-  //     window.clearInterval(i)
-  //     window.clearTimeout(i)
-  //     if (window.cancelAnimationFrame) window.cancelAnimationFrame(i)
-  //   }
-  //   this.googleUser = new GoogleUserModel()
-  // }
+  constructor() {
+    makeAutoObservable(this)
+  }
 
-  // @action
-  // public setAuthState(googleUser: GoogleUserModel): void {
-  //   this.googleUser = googleUser
-  //   Cookies.set('google_oauth2', this.googleUser, { expires: 1 })
-  // }
+  @observable 
+  public account: AccountModel = new AccountModel
+
+  @action
+  public checkLogin(): void {
+    if (Cookies.get('is_login') && window.localStorage.getItem('account')) {
+      this.account = JSON.parse(window.localStorage.getItem('account')!)
+    }
+  }
+
+  @action
+  public async getAccount(): Promise<AccountModel> {
+    let eth = (window as any).ethereum
+    let result: AccountModel = new AccountModel
+    if (eth) {
+      let accounts = await eth.request({method: 'eth_requestAccounts'})
+      let resBalance = await eth.request({method: 'eth_getBalance', params: [accounts[0], 'latest']})
+      result = {
+        tokenId: accounts[0],
+        balance: Number(Number(ethers.utils.formatEther(resBalance)).toFixed(4))
+      }
+    } else {
+      console.error('Please Install Metamask')
+    }
+    return result
+  }
+
+  @action
+  public async login(): Promise<AccountModel> {
+    this.account = await this.getAccount()
+    Cookies.set('is_login', 'true', {expires: 1})
+    window.localStorage.setItem('account', JSON.stringify(this.account))
+    return this.account
+  }
+
+  @action
+  public logout(): AccountModel {
+    if (Cookies.get('is_login')) {
+      Cookies.remove('is_login')
+    }
+    if (window.localStorage.getItem('account')) {
+      window.localStorage.removeItem('account')
+    }
+    this.account = new AccountModel
+    return this.account
+  }
+
 }
 
+export const AuthStoreContext = createContext(new AuthStore())
 const authStore: AuthStore = new AuthStore()
 export type AuthStoreType = typeof authStore
-export default authStore
+export default AuthStore
