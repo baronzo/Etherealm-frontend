b9f818c516699d0093f12343291cc6c57eab9e7c
Matupoom Chaiwan <matupoom001@gmail.com>
2022-03-01 21:44:09 +0700

    function select image in edit land page

--- src/components/EditLand/EditLand.tsx
+++ src/components/EditLand/EditLand.tsx
@@ -1,7 +1,42 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
+import LandModel from '../../models/lands/LandModel'
 import './EditLand.scss'
 
 export default function EditLand() {
+  const inputImage = useRef<HTMLInputElement>(null)
+  const [land, setLand] = useState<LandModel>(new LandModel)
+
+  function onChangeImageClick(): void {
+    inputImage.current?.click()
+  }
+
+  async function onImageSelected(e: HTMLInputElement): Promise<void> {
+    const target: HTMLInputElement = e
+    let image: FileList  = target.files as FileList 
+    let type: string = image[0].type.split('/')[1]
+    let base64Image = await convertImageToBase64(image[0])
+    let newLand = {...land}
+    newLand.landAssets = base64Image
+    setLand(newLand)
+  }
+
+  function mapImage(): string {
+    if (land.landAssets) {
+      return land.landAssets
+    }
+    return './land.png'
+  }
+
+  function convertImageToBase64(image: File): Promise<string> {
+    return new Promise(resolve => {
+      let reader = new FileReader();
+      reader.readAsDataURL(image);
+      reader.onload = () => {
+        resolve(String(reader.result))
+      };
+    });
+  }
+
   return (
     <div id='editLand'>
       <div id="editBox">
@@ -10,9 +45,10 @@ export default function EditLand() {
         </div>
         <div id="editDetail">
           <div className="image-section">
-            <img id="landImage" src="./land.png" alt=""/>
+            <img id="landImage" src={land.landAssets ? land.landAssets : './land.png'} alt=""/>
             <div id="changeImage">
-              <button className='button'>Change Image</button>
+              <input type="file" ref={inputImage} name="" id="uploadInput" onChange={e => onImageSelected(e.target)} accept="image/png, image/jpeg" />
+              <button className='button' onClick={onChangeImageClick}>Change Image</button>
             </div>
           </div>
           <div className="edit-section">
@@ -22,7 +58,7 @@ export default function EditLand() {
             </div>
             <div className="input-box">
               <div className="text">Land Description</div>
-              <input type="text" className='input-description' />
+              <textarea className='input-description' />
             </div>
             <div className="input-box">
               <div className="text">URL</div>
