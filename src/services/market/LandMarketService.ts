1cdedb5af1361b885f4c99aea3ade8bb6ccd7bee
Arnicha <arnichamichai@gmail.com>
2022-04-19 23:40:56 +0700

    post add land on market/sell

--- src/services/market/LandMarketService.ts
+++ src/services/market/LandMarketService.ts
@@ -1,5 +1,9 @@
 import axios, { AxiosResponse } from "axios"
+import AccountModel from "../../models/auth/AccountModel"
 import LandMarketModel from "../../models/lands/LandMarketModel"
+import ListLandOnMarketRequestModel from "../../models/lands/ListLandOnMarketRequestModel"
+import ListOnMarketResponseModel from "../../models/lands/ListOnMarketResponseModel"
+import authStore from "../../store/auth"
 import Host from "../Host"
 
 class LandMarketService {
@@ -8,6 +12,21 @@ class LandMarketService {
     public async getLandsOnMarket(): Promise<Array<LandMarketModel>> {
         let landsMarket: AxiosResponse<Array<LandMarketModel>> = await axios.get(`${this.host}/market/land`)
         return landsMarket.data
+    }
+
+    // public async purchaseLand(landTokenId: string): Promise<LandModel> {
+    //     let account: AccountModel = await authStore.getAccount()
+    //     let body: PurchaseLandRequestModel = {
+    //       landTokenId: landTokenId,
+    //       ownerTokenId: account.userTokenId
+    //     }
+    //     let land: AxiosResponse<LandModel> = await axios.post(`${this.host}/lands/purchase`, body)
+    //     return land.data
+    //   }
+
+    public async listLandOnMarket(listOnMarket: ListLandOnMarketRequestModel): Promise<ListOnMarketResponseModel> {
+        let bodyResponse: AxiosResponse<ListOnMarketResponseModel> = await axios.post(`${this.host}/market/land/create`, listOnMarket)
+        return bodyResponse.data
     }
 
 }
