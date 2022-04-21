3e9bd5ae7eb1867729319188603b269c9c4d6b7f
Arnicha <arnichamichai@gmail.com>
2022-04-21 23:30:45 +0700

    get land in owner/other profile

---
+++ src/models/showLand/ActiveFillterStatusModel.ts
@@ -0,0 +1,8 @@
+export default class ActiveFillterStatusModel {
+    public mapOwnedLands: boolean = Boolean();
+    public landForSellOnMarket: boolean = Boolean();
+    public landForRentOnMarket: boolean = Boolean();
+    public landRent: boolean = Boolean();
+    public landRentPurchase: boolean = Boolean();
+    public landPeopleAreRenting: boolean = Boolean();
+}
