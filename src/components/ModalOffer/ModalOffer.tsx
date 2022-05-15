f6a1f7752a61948371370beeb8694cfef6ae0d12
udomsak <specialclub13@gmail.com>
2022-05-15 17:46:13 +0700

    map best offer

--- src/components/ModalOffer/ModalOffer.tsx
+++ src/components/ModalOffer/ModalOffer.tsx
@@ -22,7 +22,7 @@ export default function ModalOffer(props: Props) {
   const offerService: OfferService = new OfferService
 
   useEffect(() => {
-    getOfferLand()
+    
 }, [])
 
   async function createOfferLand(): Promise<void> {
@@ -35,6 +35,7 @@ export default function ModalOffer(props: Props) {
     const result: CreateOfferLandResponseModel = await offerService.createOffer(body)
     console.log(result)
     setIsLoading(false)
+    props.setIsShowModalOffer(false)
   }
 
   function onChangeOfferPrice(e: React.ChangeEvent<HTMLInputElement>) {
@@ -44,18 +45,6 @@ export default function ModalOffer(props: Props) {
     } else if (value >= 0.00001) {
       setOfferPrice(e.target.value)
     }
-  }
-
-  async function getOfferLand(): Promise<void> {
-    const body: OffersLandRequestModel = {
-      landTokenId: props.landOffer.landTokenId,
-      page: 1,
-      sortBy: 1
-    }
-    const result: OffersLandResponseModel = await offerService.getOffersLandByLandTokenId(body)
-    setOfferResponse(result)
-    console.log(offerResponse)
-    // console.log(result)
   }
 
   return (
@@ -73,14 +62,15 @@ export default function ModalOffer(props: Props) {
         <div id="landNameBox">
           <p className='land-name'>{props.landOffer.landName}</p>
         </div>
+        {props.landOffer.bestOffer &&
         <div id="bestOfferBox">
           <div className="best-offer">
             <div className="profile">
-              <img className="image-profile" src="/profile.jpg" alt="" />
+              <img className="image-profile" src={`${props.landOffer.bestOffer?.fromUserTokenId.userProfilePic ? props.landOffer.bestOffer?.fromUserTokenId.userProfilePic : '/profile.jpg' }`} alt="" />
             </div>
             <div className="detail-best-offer">
               <div className="text-name">
-                <p className='name'>Udomsak</p>
+                <p className='name'>{props.landOffer.bestOffer?.fromUserTokenId.userName}</p>
                 <i className="fas fa-crown icon"></i>
               </div>
               <div className="user-token">
@@ -91,11 +81,12 @@ export default function ModalOffer(props: Props) {
               </div>
               <div className="tag-best-offer">
                 <i className="fab fa-ethereum ether"></i>
-                <p className='best-price'>Best offer: 999 eth</p>
+                <p className='best-price'>Best offer: {props.landOffer.bestOffer?.offerPrice ? props.landOffer.bestOffer?.offerPrice : 0.00 } eth</p>
               </div>
             </div>
           </div>
         </div>
+        }
         <div id="offerPriceBox">
           <div className="text-price-box">
             <p className='text-bold'>Offer Price</p>
@@ -108,12 +99,12 @@ export default function ModalOffer(props: Props) {
         <div id="billBox">
           <div className="bill-text">
             <p className='text-format'>Platform Fee (2.5%)</p>
-            <p className='text-format'>{props.landOffer?.bestOffer?.fees} ETH</p>
+            <p className='text-format'>{Number(offerPrice) * 0.025} ETH</p>
           </div>
           {console.log(props.landOffer)}
           <div className="bill-text">
             <p className='text-format'>You will receive</p>
-            <p className='text-format'>{(Number(offerPrice)).toFixed(6)}  ETH</p>
+            <p className='text-format'>{(Number(offerPrice) - Number(offerPrice) * 0.025).toFixed(6)} ETH</p>
           </div>
         </div>
         <div id="buttonBox">
