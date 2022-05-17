5d0ec3629f95af3862f1547736eff1103ffe9064
Arnicha <arnichamichai@gmail.com>
2022-05-17 20:05:41 +0700

    post api hire purchase

--- src/components/ModalHirePurchase/ModalHirePurchase.tsx
+++ src/components/ModalHirePurchase/ModalHirePurchase.tsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ModalHirePurchase.scss";
 import { MdClose } from "react-icons/md";
 import LandModel from "../../models/lands/LandModel";
 import Select from 'react-select'
+import HirePurchaseService from "../../services/hirePurchase/HirePurchaseService";
+import HirePurchasePostRequestModel from "../../models/hirePurchase/HirePurchasePostRequestModel";
+import ReactSelectOptionModel from "../../models/reactSelect/ReactSelectOptionModel";
+import HirePurchasePostResponseModel from "../../models/hirePurchase/HirePurchasePostResponseModel";
 
 type Props = {
   landDetails: LandModel;
@@ -10,14 +14,22 @@ type Props = {
 };
 
 interface Options {
-  value: number,
-  label: string
+  value: number | undefined;
+  label: string | undefined;
 }
 
 export default function ModalHirePurchase(props: Props) {
   const [isLoading, setisLoading] = useState<boolean>(false);
+  const hirePurchaseService = new HirePurchaseService()
+  const [period, setPeriod] = useState<Options>({ value: 1, label: '1' })
+  const [platformFees, setPlatformFees] = useState<number>(0)
+  const [interest, setInterest] = useState<number>(0)
+  const [pay, setPay] = useState<number>(0)
+  const [fees, setFees] = useState<number>(0)
+  const [checked, setChecked] = useState<boolean>(false); 
+  const [monthly, setMonthly] = useState<number>(0)
 
-  const options: Array<Options> = [
+  const [optionsPeroid, setOptionsPeroid] = useState<Array<Options>>([
     { value: 1, label: '1' },
     { value: 2, label: '2' },
     { value: 3, label: '3' },
@@ -30,7 +42,80 @@ export default function ModalHirePurchase(props: Props) {
     { value: 10, label: '10' },
     { value: 11, label: '11' },
     { value: 12, label: '12' }
-  ]
+  ])
+
+  useEffect(() => {
+    console.log(props.landDetails)
+    calculate()
+  }, [period.value])
+
+
+  const mapPeriodToOption = (): Array<ReactSelectOptionModel> => {
+    const options: Array<ReactSelectOptionModel> = new Array<ReactSelectOptionModel>()
+    optionsPeroid.forEach((period) => {
+      const reactSelectOption: ReactSelectOptionModel = new ReactSelectOptionModel()
+      reactSelectOption.label = period.label
+      reactSelectOption.value = period.value
+      options.push(reactSelectOption)
+    })
+    return options
+  }
+
+  const mapEventPeriodToOption = (e: ReactSelectOptionModel): void => {
+    const reactSelectOption: ReactSelectOptionModel = e
+    let newPeriod = new ReactSelectOptionModel
+    newPeriod!.label = reactSelectOption.label
+    newPeriod!.value = reactSelectOption.value
+    setPeriod(newPeriod)
+  }
+
+  async function postHirePuechaseLand(): Promise<void> {
+    setisLoading(true)
+    const bodyRequest: HirePurchasePostRequestModel = {
+      hash: '0x9f526330448a8343a39969b0635ec589b3e7be46c4e9896856f5614c13c6041e',
+      landTokenId: props.landDetails.landTokenId!,
+      period: period.value! * 30,
+      price: 1
+      // fees: fees
+    }
+    if(checked) {
+      console.log(bodyRequest)
+      const hirePuechaseResponse: HirePurchasePostResponseModel = await hirePurchaseService.postHirePurchaseLand(bodyRequest)
+      if (hirePuechaseResponse) {
+        console.log(hirePuechaseResponse)
+        setisLoading(false)
+        props.setIsShowModalHirePurchase(false)
+        window.location.reload()
+      }
+    }
+  }
+
+  async function confirmHirePurchase() {
+    await postHirePuechaseLand()
+  }
+
+  const calculate = ():void => {
+    let willPay: number = 0
+    let monthlyPay: number = props.landDetails.price! / period.value!
+    setMonthly(monthly)
+    let platform: number = monthlyPay * 0.25
+    let interest: number = monthlyPay * 0.01
+    let fees: number = platform + interest
+    if (period.value! <= 3) {
+      willPay = monthlyPay + platformFees
+      setInterest(0)
+      setPay(willPay)
+    }
+    willPay = monthlyPay + platformFees + interest
+    setFees(fees)
+    setPlatformFees(platform)
+    setInterest(interest)
+    setPay(willPay)
+  }
+
+  const handleChange = ():void => { 
+    setChecked(!checked); 
+  }; 
 
   return (
     <div id="hirePurchaseBox">
@@ -44,11 +129,7 @@ export default function ModalHirePurchase(props: Props) {
         <div className="image-div">
           <img
             className="image-profile"
-            src={
-              props.landDetails.landAssets
-                ? props.landDetails.landAssets
-                : "/map.jpg"
-            }
+            src={props.landDetails.landAssets? props.landDetails.landAssets: "/map.jpg"}
             alt=""
           />
         </div>
@@ -57,13 +138,7 @@ export default function ModalHirePurchase(props: Props) {
       <div className="name-description">
         <div className="name-input-div">
           <p className="label-name">Period(Month)</p>
-          <select name="selectPeriod" id="selectPeriod" className="input-select">
-            {options.map((item: Options) => {
-              return(
-                <option key={item.value} value={item.value}>{item.label}</option>
-              )
-            })}
-          </select>
+          <Select value={period} options={(mapPeriodToOption())} onChange={(e) => mapEventPeriodToOption(e as ReactSelectOptionModel)} />
         </div>
         <div className="fee-div">
           <div className="fee-item">
@@ -71,36 +146,36 @@ export default function ModalHirePurchase(props: Props) {
               <p className="fee-label-text">Platform Fee (2.5%)</p>
             </div>
             <div className="fee-value">
-              <p className="fee-value-text"> ETH</p>
+              <p className="fee-value-text">{platformFees} ETH</p>
             </div>
           </div>
           <div className="fee-item">
             <div className="fee-label">
-              <p className="fee-label-text">Platform Fee (2.5%)</p>
+              <p className="fee-label-text">Interest (1%)</p>
             </div>
             <div className="fee-value">
-              <p className="fee-value-text"> ETH</p>
+              <p className="fee-value-text">{interest} ETH</p>
             </div>
           </div>
           <div className="fee-item">
             <div className="fee-label">
-              <p className="fee-label-text">You will receive</p>
+              <p className="fee-label-text">You will pay</p>
             </div>
             <div className="fee-value">
-              <p className="fee-value-text"> ETH</p>
+              <p className="fee-value-text">{pay} ETH / Month</p>
             </div>
           </div>
         </div>
       </div>
       <div className="checkbox">
         <div className="checkbox-div">
-          <input className="checkbox-icon" type="checkbox"/>
+          <input className="checkbox-icon" type="checkbox" onChange={handleChange}/>
           <p className="accept">Accept the terms of use</p>
-          </div>
+        </div>
       </div>
       <div className="button-save-div">
         {!isLoading ? (
-          <button className="button-save" onClick={() => {}}>
+          <button className="button-save" onClick={() => confirmHirePurchase()}>
             Confirm
           </button>
         ) : (
--- src/services/hirePurchase/HirePurchaseService.ts
+++ src/services/hirePurchase/HirePurchaseService.ts
@@ -1,5 +1,7 @@
 import axios, { AxiosResponse } from "axios"
 import HirePurchaseDetailResponseModel from "../../models/hirePurchase/HirePurchaseDetailResponseModel"
+import HirePurchasePostRequestModel from "../../models/hirePurchase/HirePurchasePostRequestModel"
+import HirePurchasePostResponseModel from "../../models/hirePurchase/HirePurchasePostResponseModel"
 import Host from "../Host"
 
 class HirePurchaseService {
@@ -9,6 +11,12 @@ class HirePurchaseService {
     let response: AxiosResponse<HirePurchaseDetailResponseModel> = await axios.get(`${this.host}lands/hire/details/${landTokenId}`)
     return response.data
   }
+
+  public async postHirePurchaseLand(bodyRequest: HirePurchasePostRequestModel): Promise<HirePurchasePostResponseModel> {
+    let response: AxiosResponse<HirePurchasePostResponseModel> = await axios.post(`${this.host}/lands/hire`, bodyRequest)
+    return response.data
+  }
+
 }
 
 export default HirePurchaseService
