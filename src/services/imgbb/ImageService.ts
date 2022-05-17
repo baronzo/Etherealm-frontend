0a3d1af55aa51fdb802d6335edc8742994ecb9ff
Matupoom Chaiwan <matupoom001@gmail.com>
2022-05-17 14:43:04 +0700

    fix image service cors and edit style in modal profile

--- src/services/imgbb/ImageService.ts
+++ src/services/imgbb/ImageService.ts
@@ -4,10 +4,12 @@ import ImageModel from "../../models/imgbb/ImageModel"
 class ImageService {
 
   public async postImageApi(imageBase64: string): Promise<string> {
-    const path: string = 'https://api.imgbb.com/1/upload?expiration=604800&key=27a07cc9c38cccf83c919bb18bda4fb7'
+    const path: string = 'https://api.imgbb.com/1/upload?expiration=2592000&key=27a07cc9c38cccf83c919bb18bda4fb7'
     const formData: FormData = new FormData
     formData.append('image', imageBase64)
-    let result: AxiosResponse<ImageModel> = await axios.post<ImageModel>(path, formData)
+    let instance = axios.create();
+    delete instance.defaults.headers.common['Authorization'];
+    let result: AxiosResponse<ImageModel> = await instance.post<ImageModel>(path, formData)
     return result.data.data.display_url
   }
 
