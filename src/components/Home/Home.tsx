ae622db7c59fe1001562e0ceb13bd78ef7ad3604
Arnicha <arnichamichai@gmail.com>
2022-05-18 15:36:12 +0700

    home page

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,32 +1,52 @@
 import React, { useEffect } from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useHistory } from 'react-router-dom'
 import './Home.scss'
 
 const Home = () => {
+  const history = useHistory()
+
+  function goToMarketPage(): void {
+    history.push(`/market`)
+  }
+
+  function goToMapPage(): void {
+    history.push(`/map`)
+  }
 
   return (
     <>
       <div id="home">
-        <img src="" alt="" width="200px" height="200px"/>
-        <div id="text">Lorem ipsum</div>
+        <div className='image-div'>
+          <img src="/image1.png" alt="" className="image-main" />
+        </div>
+        <div id="text">ETHEREALM</div>
         <div id="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur temporibus nostrum animi non voluptate</div>
         <div className="button">
-          <button className="wallet">Connect to a wallet</button>
-        <button className="map">
-          <NavLink to={'/map'}>View Map</NavLink>
-        </button>
+          <button className="map" onClick={goToMapPage}>View Map</button>
         </div>
       </div>
-      <div id="auction">Auction here</div>
-      <div id="land">
-        <div className="image">
-          <img src="" alt="" width="200" height="200" />
+      <div id="landMarket">
+        <div className="image-matket-div">
+          <img className='image-market' src="/imag2.png" alt="" />
         </div>
         <div className="land-box">
-          <div className="title">Why Etherealm ?</div>
-          <div className="text-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur temporibus nostrum animi non voluptate</div>
-          <button className="buy-land">Buy Land</button>
+          <div className="title">Market in ETHEREALM</div>
+          <div className="text-description">$SAND is our main utility token that allows you to buy and sell LANDS and ASSETS in The Sandbox metaverse.</div>
+          <button className="buy-land" onClick={goToMarketPage}>Buy Land</button>
         </div>
+      </div>
+      <div id="landRent">
+        <div className="land-box">
+          <div className="title">Rent in ETHEREALM</div>
+          <div className="text-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur temporibus nostrum animi non voluptate</div>
+          <button className="buy-land" onClick={goToMarketPage}>Rent Land</button>
+        </div>
+        <div className="image-matket-div">
+          <img className='image-market' src="/image3.png" alt="" />
+        </div>
+      </div>
+      <div id="footer">
+
       </div>
     </>
   )
