99ee4ab2ed9ec7254c8015ab9564e4f8dc8e002f
Arnicha <arnichamichai@gmail.com>
2022-05-23 16:59:11 +0700

    notify offer

--- src/components/ModalOffer/ModalOffer.tsx
+++ src/components/ModalOffer/ModalOffer.tsx
@@ -39,11 +39,11 @@ export default function ModalOffer(props: Props) {
       setIsLoading(false)
       props.setIsShowModalOffer(false)
       props.fetchOffer()
-      Notify.notifySuccess('Offer this land successfully')
+      Notify.notifySuccess(`offer land ${props.landOffer.landName} successfully`)
     } catch (error) {
       console.error(error)
       setIsLoading(false)
-      Notify.notifyError('Offer this land failed !!')
+      Notify.notifyError(`offer land ${props.landOffer.landName} failed !!`)
     }
   }
 
